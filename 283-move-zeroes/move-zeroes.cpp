5b9a7eb1b13dddba59b6a88b9282be87b084b6b3
Bharat <bdangi450@gmail.com>
2026-02-08 17:20:41 +0530

    Refactor moveZeroes implementation and enhance main function for user input

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,14 +1,53 @@
-class Solution {
+#include <bits/stdc++.h>
+using namespace std;
+
+// -------- Solution Class --------
+class Solution
+{
 public:
-    void moveZeroes(vector<int>& nums) {
+    void moveZeroes(vector<int> &nums)
+    {
         int n = nums.size();
         int j = 0;
 
-        for (int i = 0; i < n; i++) {
-            if(nums[i] != 0) {
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] != 0)
+            {
                 swap(nums[i], nums[j]);
                 j++;
             }
         }
     }
 };
+
+// -------- Driver / Main --------
+int main()
+{
+    Solution obj;
+
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    // Call function
+    obj.moveZeroes(nums);
+
+    // Output result
+    cout << "Array after moving zeroes:\n";
+    for (int x : nums)
+    {
+        cout << x << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
